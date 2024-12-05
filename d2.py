4ec0b27680186e1c8ecb7e0deff737061b2a89d8
Kendall Bailey <kbailey@figure.com>
2024-12-05 16:32:05 +0000

    tweak day 2

--- d2.py
+++ d2.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-import itertools
+import itertools as itl
 
-txt = Path('d2.txt').read_text()
-reports = [[int(x) for x in line.split()] for line in txt.strip().split('\n') if line]
+txt = Path('d2.txt').read_text().strip()
+reports = [[int(x) for x in line.split()] for line in txt.split('\n')]
 
 def issafe(lvls):
-   diffs = [a-b for a, b in zip(lvls, itertools.islice(lvls, 1, None))]
+   diffs = [a-b for a,b in zip(lvls, itl.islice(lvls, 1, None))]
    if all(x<0 for x in diffs) or all(x>0 for x in diffs):
       if all(1 <= abs(x) <= 3 for x in diffs):
          return True
@@ -14,11 +14,10 @@ safe = list(filter(issafe, reports))
 
 print(len(safe))
 
-def rm(lvls, n):
-    return lvls[:n] + lvls[n+1:]
+rm = lambda lvls, n: lvls[:n] + lvls[n+1:]
 
-def issafe2(lvls):
-    return any(issafe(x) for x in (rm(lvls, n) for n in range(-1, len(lvls))))
+issafe2 = lambda lvls: any(issafe(x) for x in (rm(lvls, n)
+                           for n in range(-1, len(lvls))))
 
 safe = list(filter(issafe2, reports))
 
