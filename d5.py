d879d0b03000f367d4c3621ca5946b9443811032
Kendall Bailey <kbailey@figure.com>
2024-12-05 20:34:11 +0000

    tweak day 5

--- d5.py
+++ d5.py
@@ -4,8 +4,8 @@ from itertools import combinations as C
 
 p = plb.Path((sys.argv+['d5.txt'])[1]).read_text().strip().split('\n\n')
 rls, pr = [x.split('\n') for x in p]
-b4 = {tuple(int(x) for x in line.split('|')) for line in rls}
-upd = [[int(v) for v in ln.split(',')] for ln in pr]
+b4 = {tuple(map(int,ln.split('|'))) for ln in rls}
+upd = [list(map(int,ln.split(','))) for ln in pr]
 
 bad = lambda pn: any((b,a) in b4 for a,b in C(pn, 2))
 
