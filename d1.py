403748e704ed6c9482a63e8dac99d984ed7ecbbb
Kendall Bailey <kbailey@figure.com>
2024-12-05 16:27:55 +0000

    tweak day 1

--- d1.py
+++ d1.py
@@ -1,15 +1,11 @@
 from pathlib import Path
 from collections import Counter
 
-txt = Path('d1.txt').read_text()
-nums = [[int(x.strip()) for x in line.split()] for line in txt.split('\n') if line]
+txt = Path('d1.txt').read_text().strip()
+nums = [[int(x) for x in line.split()] for line in txt.split('\n')]
 
-cols = list(zip(*nums))
-first = sorted(cols[0])
-second = sorted(cols[1])
-
-dist = sum(abs(a-b) for a,b in zip(first, second))
+cols = [sorted(x) for x in zip(*nums)]
+dist = sum(abs(a-b) for a,b in zip(*cols))
 print(dist)
-
-c = Counter(second)
-print(sum(x*c[x] for x in first))
+c = Counter(cols[1])
+print(sum(x*c[x] for x in cols[0]))
