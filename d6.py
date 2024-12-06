34f6792eb8c0729077474549ec801b37972d621d
Kendall Bailey <kbailey@figure.com>
2024-12-06 15:24:13 +0000

    tweak day 6

--- d6.py
+++ d6.py
@@ -1,40 +1,31 @@
-import pathlib as pl
-import itertools as itl
-import sys
+import sys as S, pathlib as P, itertools as I 
 
-grid = list(map(list, pl.Path((sys.argv+['d6.txt'])[1]).read_text().strip().split('\n')))
+grid = list(map(list,P.Path((S.argv+['d6.txt'])[1]).read_text().strip().split('\n')))
+R, X, Y, *stepd = range, len(grid), len(grid[0]), -1, 1j, 1, -1j
+G = lambda: I.starmap(complex, I.product(R(X), R(Y)))
 
-X, Y = len(grid), len(grid[0])
-stepd = (-1, 1j, 1, -1j)
+atpos=lambda p:0<=(i:=int(p.real))<X and 0<=(j:=int(p.imag))<Y and grid[i][j] or ''
 
-atpos = lambda p: grid[int(p.real)][int(p.imag)] if 0 <= p.real < X and 0 <= p.imag < Y else ' '
 def put(p, c): grid[int(p.real)][int(p.imag)] = c
 
-start = [p for a,b in itl.product(range(X), range(Y)) if atpos(p := complex(a,b)) == '^'][0]
+start = next(filter(lambda p: atpos(p)=='^', G()))
 
 def walk(pos, d):
-    vs = set()
-    loop = set()
+    vs, loop = [], set()
     while True:
-        if (pos, d) in loop:
-            return vs, True
-        vs.add(pos); loop.add((pos, d))
+        if (pos, d) in loop: return vs, True
+        vs.append(pos); loop.add((pos, d))
         match atpos(pos+stepd[d]):
-            case ' ': break
-            case '#': d = (d+1)%4 
+            case '': break
+            case '#': d = (d+1)%4  # right turn
             case _: pos = pos+stepd[d]
     return vs, False
 
-print(len(walk(start, 0)[0]))
+route = walk(start, 0)[0]
+print(len(set(route)))
 
 def walk2(pos, d, ad):
-    old = atpos(ad)
-    put(ad, '#')
-    result = walk(pos, d)
-    put(ad, old)
-    print(ad, result[1])
-    return result
-    
-print(sum(1 for a, b in itl.product(range(X), range(Y))
-      if atpos(p:=complex(a,b)) == '.' and walk2(start, 0, p)[1]))
+    return (put(ad, '#'), walk(pos, d), put(ad, '.'))[1]
+
+print(sum(1 for p in G() if atpos(p) == '.' and walk2(start, 0, p)[1]))
 
