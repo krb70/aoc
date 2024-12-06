d8b074e028e99d1cbc50a06ee58d2e12f3aa3a0c
Kendall Bailey <kbailey@figure.com>
2024-12-06 16:59:58 +0000

    Improve day 6 performance

--- d6.py
+++ d6.py
@@ -1,20 +1,20 @@
-import sys as S, pathlib as P, itertools as I 
+import sys as S, pathlib as P, itertools as I, collections as C
 
 grid = list(map(list,P.Path((S.argv+['d6.txt'])[1]).read_text().strip().split('\n')))
 R, X, Y, *stepd = range, len(grid), len(grid[0]), -1, 1j, 1, -1j
 G = lambda: I.starmap(complex, I.product(R(X), R(Y)))
-
-atpos=lambda p:0<=(i:=int(p.real))<X and 0<=(j:=int(p.imag))<Y and grid[i][j] or ''
+_atpos=lambda p:0<=(i:=int(p.real))<X and 0<=(j:=int(p.imag))<Y and grid[i][j] or ''
+atpos = C.defaultdict(str, {p:_atpos(p) for p in G()})
 nxt=lambda p,d: p+stepd[d]
-def put(p, c): grid[int(p.real)][int(p.imag)] = c
+put = atpos.__setitem__
 
-start = next(filter(lambda p: atpos(p)=='^', G()))
+start = next(filter(lambda p: atpos[p]=='^', G()))
 
 def walk(pos, d):
     vs, loop = [pos], set()
     while True:
         if pos != vs[-1]: vs.append(pos)
-        match atpos(n:=nxt(pos,d)):
+        match atpos[n:=nxt(pos,d)]:
             case '': break
             case '#':
                 d = (d+1)%4  # right turn
@@ -30,5 +30,5 @@ print(len(set(route)))
 def walk2(pos, d, ad):
     return (put(ad, '#'), walk(pos, d), put(ad, '.'))[1]
 
-print(sum(1 for p in set(route)-{start} if atpos(p)=='.' and not walk2(start, 0, p)))
+print(sum(1 for p in set(route)-{start} if atpos[p]=='.' and not walk2(start, 0, p)))
 
