de3b20d3bed254f319ce645c4417ccc054af1d75
Kendall Bailey <kbailey@figure.com>
2024-12-10 15:54:51 +0000

    tweak day 10

--- d10.py
+++ d10.py
@@ -1,18 +1,17 @@
-import pathlib as P, sys as S, itertools as I
+import pathlib as P, sys as S, itertools as I, collections as O
 R, C = range, complex
 _ = P.Path((S.argv+['d10.txt'])[1]).read_text().strip().split('\n')
-G = {C(i,j):int(_[i][j]) for i,j in I.product(R(len(_)), R(len(_[0])))} 
-paths, d = {}, {-1j**n for n in R(4)}
+G = {C(i,j): int(_[i][j]) for i,j in I.product(R(len(_)), R(len(_[0])))} 
+paths, D = O.defaultdict(set), {-1j**n for n in R(4)}
 
 heads = set(k for k,v in G.items() if not v)
 
 def search(p):
     if (v:=G[p]) == 9: paths[p] = {(p,)}
-    for s in d:
+    for s in D:
         if G.get((nxt:=p+s), -1) != v+1: continue
         if nxt not in paths: search(nxt)
-        subpaths = {(p,) + tail for tail in paths.get(nxt,set())}
-        paths[p] = paths.get(p, set()) | subpaths
+        paths[p] |= {(p,) + tail for tail in paths[nxt]}
 
 for h in heads: search(h)
 # endpoints
