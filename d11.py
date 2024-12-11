ca7b2d6a3d2f1802a55d5204a22fecaed563e7ba
Kendall Bailey <kbailey@figure.com>
2024-12-11 14:11:23 +0000

    simplify day 11

--- d11.py
+++ d11.py
@@ -1,13 +1,10 @@
 import sys as S, pathlib as P, functools as F
 T = list(map(int,P.Path((S.argv+['d11.txt'])[1]).read_text().strip().split()))
 
-@F.cache
-def stonecnt(s, n):
-    if not n: return 1
-    if not s: return stonecnt(1, n-1)
-    if (k:=len(d:=str(s)))%2: return stonecnt(2024*s, n-1)
-    return stonecnt(int(d[:k//2]),n-1) + stonecnt(int(d[k//2:]),n-1) 
-
-print(sum(stonecnt(s, 25) for s in T))
-print(sum(stonecnt(s, 75) for s in T))
-
+N = F.cache(lambda s,n:
+       ((o:=n-1)<0 and 1) or (not s and N(1,o)) or
+       ((k:=len(d:=str(s)))%2 and N(2024*s,o)) or
+       N(int(d[:(m:=k//2)]),o)+N(int(d[m:]),o)
+    )
+print(sum(N(s, 25) for s in T))
+print(sum(N(s, 75) for s in T))
