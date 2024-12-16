ac3fbc8497d44248baff1e0891a86e1f80f4ef09
Kendall Bailey <kendall@baileyplex.com>
2024-12-16 08:56:07 -0600

    tweak day 16

--- d16.py
+++ d16.py
@@ -1,6 +1,6 @@
 import sys as S, pathlib as P, itertools as I, types
 from collections import deque 
-R, C, NS, T = range, complex, types.SimpleNamespace, -1j
+R, C, NS, T = range, complex, types.SimpleNamespace, {-1j**k for k in range(4)}
 _ = P.Path((S.argv+['d16.txt'])[1]).read_text().strip().split('\n')
 G={C(x,y):c for x,y in I.product(R(len(_)),R(len(_[0]))) if (c:=_[x][y])!='#'}  
 S=next((p for p,v in G.items() if v=='S'))
@@ -13,8 +13,7 @@ def bfs():
         if (done:=G[p]=='E'): res.append(NS(cost=cst, rt=rt))
         if done or seen.get((p,d),cst) < cst: continue
         seen[p,d] = cst
-        for nd in (d*T**k for k in (1,3,4)): # right, left, forward
-            if (n:=p+nd) in G and n not in s:
+        for nd in (td for td in T if (n:=p+td) in G and n not in s):
                 np,ra,ca = d==nd and (n,[n],1) or (p,[],1000)
                 q.append((cst+ca,nd,rt+ra,s|set(ra),np))
     return res
