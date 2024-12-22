195145fa56d72d0643caecdd18d6af5b0c3a7404
Kendall Bailey <kendall@baileyplex.com>
2024-12-22 11:48:09 -0600

    tweak day 22

--- d22.py
+++ d22.py
@@ -1,18 +1,14 @@
-import sys as S, pathlib as P, collections as O 
+import sys as S, pathlib as P, collections as O, itertools as I
 vals=list(map(int,P.Path((S.argv+['d22.txt'])[1]).read_text().strip().split()))
-U,R,EN,D=16777216,range,enumerate,O.deque
+N,U,D,PW,T,SD=2000,2**24-1,O.deque,I.pairwise,tuple,dict.setdefault
 
-rn = lambda x: (((x:=(((x:=((x<<6)^x)%U)>>5)^x)%U)<<11)^x)%U
-rnk = lambda x,k: D((x:=rn(x) for _ in R(k)), 1)[0]
-
-print(sum(rnk(v,2000) for v in vals))
+rn = lambda x: (((x:=(((x:=((x<<6)^x)&U)>>5)^x)&U)<<11)^x)&U
+rnk = lambda x,k: I.chain((x,),(x:=rn(x) for _ in range(k)))
+print(sum(D(rnk(v,N),1)[0] for v in vals))
 
 s2b = O.defaultdict(dict)
-for n,v in EN(vals):
-    d,x,p = D((),4),v,v
-    for i in R(2000):
-        ch,p=((x:=rn(p))%10)-(p%10),x
-        d.append(ch)
-        if len(d)==4: s2b[tuple(d)].setdefault(n, x%10)
+for n,v in enumerate(vals):
+    d = D((),4)
+    D((SD(s2b[T(d)],n,b) for p,x in PW(rnk(v,N)) if d.append((b:=x%10)-p%10) or len(d)==4),1)
 
 print(max(sum(v.values()) for v in s2b.values()))
